43fa496c8e28057ee2a6f709459ec41d11200244
VijayaBausch <vijaya.vilayatkar@bausch.com>
2016-11-21 15:45:10 +0530

    Added the Task factory function in order ot create Task Class

--- TaskExample/Program.cs
+++ TaskExample/Program.cs
@@ -10,26 +10,22 @@ namespace TaskExample
     class Program
     {
         static void Main(string[] args)
-        {
-            var t1 = new Task(() => DoSomeVeryImportantWork(1,1500));
+        {   
+            var t1 =  Task.Factory.StartNew(() => DoSomeVeryImportantWork(1,1500));
 
-            t1.Start();
+            var t2 =  Task.Factory.StartNew(() => DoSomeVeryImportantWork(2, 3000));
 
-            for (int count = 0; count < 100; count++)
-            {
-                Console.WriteLine("Printing more values  from" + count);
-            }
+            var t3 =  Task.Factory.StartNew(() => DoSomeVeryImportantWork(3, 1000));
 
-            Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
-
+         
         static void DoSomeVeryImportantWork(int id, int sleeptime)
         {
             Console.WriteLine("Task {0} is beginning ",id);
             for (int count = 0; count < 100;count++ )
             {
-                Console.WriteLine("From the function {0}",count);
+                Console.WriteLine("From the function {0}",id);
             }
             Console.WriteLine("Task {0} has completed", id);
         }
