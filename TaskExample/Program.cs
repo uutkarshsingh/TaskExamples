89b46a0601b3dc6a2df1ea87c7053d65d547a22e
VijayaBausch <vijaya.vilayatkar@bausch.com>
2016-11-21 15:30:07 +0530

    First commit.Added simple code to work with the task parallel library

---
+++ TaskExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Task(() => {
+                Console.WriteLine("Task1 is beginning");
+                for (int count = 0; count < 100; count++)
+                {
+                    Console.WriteLine("INSDE THREAD " + count);
+                }
+
+                    Console.WriteLine("task 1 has completed");
+            });
+
+            t1.Start();
+            for (int count = 0; count < 100; count++)
+            {
+                Console.WriteLine("Printing morev values  from" + count);
+               }
+
+                Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
